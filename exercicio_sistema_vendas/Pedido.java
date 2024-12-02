ef26de932c848b3df0aceae3cbafefa08fed8884
Chrystina <144075668+chryssstina@users.noreply.github.com>
2024-12-02 16:48:15 -0300

    Update Pedido.java
    1. Colocando o atributo pagamento em comentário;
    2. Adicionando o get/ set de List<Produto> itens;
    3. Adicionando o método para exibir a lista de produtos de pedido

--- exercicio_sistema_vendas/Pedido.java
+++ exercicio_sistema_vendas/Pedido.java
@@ -5,7 +5,7 @@ public class Pedido {
     private String idPedido;
     private Cliente cliente;
     private List<Produto> itens;
-    private Pagamento pagamento;
+    //private Pagamento pagamento;
 
     public Pedido(String idPedido, Cliente cliente) {
         this.idPedido = idPedido;
@@ -29,24 +29,40 @@ public class Pedido {
         this.cliente = cliente;
     }
     
+    public List<Produto> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<Produto> itens) {
+        this.itens = itens;
+    }
+    
     //adicionando um item-produto a um pedido do cliente
     public void addProduto(Produto produto){
         itens.add(produto);
         produto.addPedidosComProduto(this);
     }
 
+    //exibindo a lista de produtos que estão presentes em um pedidos
+    public void exibirListaProdutosdoPedido(){
+        System.out.println("O  pedido " +getIdPedido()+ " tem os itens : ");
+        for (Produto itemAtual : itens) {
+            System.out.println(itemAtual.getNome());
+        }
+    }
+    
     public double valorTotal(){
         double valorTotal = 0.0;
         for (Produto produtoAtual : itens) {
             valorTotal += produtoAtual.getPreco();
        }
-       System.out.println("Valor total: "+valorTotal);
        return valorTotal;
     }
 
     public void exibirInformacoesPedido(){
         System.out.println("ID do pedido: "+getIdPedido());
         System.out.println("Pedido feito por: "+getCliente().getNome());
+        System.out.println("Valor total: "+valorTotal());
+
     }
 }
-
