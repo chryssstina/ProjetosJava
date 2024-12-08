6f62a4047ceb29ba52275e45af0e8f36f2344ac9
Chrystina <144075668+chryssstina@users.noreply.github.com>
2024-12-07 22:10:03 -0300

    Delete sistema_monitoramento_mudancas_climaticas directory

--- sistema_monitoramento_mudancas_climaticas/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String args[]){
-        System.out.println("ola");
-
-        FenomenoClimatico fenomeno1 = new FenomenoClimatico("chuva", 7.5 , 0.3);
-    }
-}
