46e5ac01b4e45689e89e9267e778106118ec875b
Chrystina <144075668+chryssstina@users.noreply.github.com>
2024-12-08 02:01:38 -0300

    Update Main.java

--- monitoramento_de_mudancas_climaticas/Main.java
+++ monitoramento_de_mudancas_climaticas/Main.java
@@ -1,1 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        FenomenoClimatico fenomeno1 = new FenomenoClimatico("Chuva", 3.9, "45 minutos");
+        AreaGeografica area1 = new AreaGeografica("Suassurana", 150);
+
+
+       
+        fenomeno1.addArea(area1);
+        fenomeno1.calcularImpactoAmbiental();
+        fenomeno1.gerarRelatorio();
+
+
+        System.out.println("-------------------------");
+        
+        area1.addFenomeno(fenomeno1);
+        area1.gerarRelatorio();
+
+        System.out.println("-------------------------");
+
+        Relatorio relatorio1 = new Relatorio("123abc", "2024-12-08");
+        relatorio1.gerarRelatorio();
+
+
+    }
+}
 
