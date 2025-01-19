98f09199d2d934c5750ecd45ab501c23adbf0066
Chrystina <144075668+chryssstina@users.noreply.github.com>
2025-01-19 14:26:55 -0300

    Update ContaCorrente.java
    Add os métodos valorTotalPermitidoSaque() e calculoLimiteSaque()

--- Sistema-Bancario/Sistema-Bancario/src/ContaCorrente.java
+++ Sistema-Bancario/Sistema-Bancario/src/ContaCorrente.java
@@ -1,18 +1,35 @@
 public class ContaCorrente extends Conta{
-    private double limiteSaque;
+    private double limiteExtraSaque;
 
-    public ContaCorrente(String idConta, Cliente cliente, double saldoEmConta, double limiteSaque){
+    public ContaCorrente(String idConta, Cliente cliente, double saldoEmConta){
         super(idConta, cliente, saldoEmConta);
-        this.limiteSaque = limiteSaque;
+        this.limiteExtraSaque = limiteExtraSaque;
     }
 
     
-    public double getLimiteSaque() {
-        return limiteSaque;
+    public double getLimiteExtraSaque() {
+        return limiteExtraSaque;
     }
-    public void setLimiteSaque(double limiteSaque) {
-        this.limiteSaque = limiteSaque;
+    public void setLimiteExtraSaque(double limiteExtraSaque) {
+        this.limiteExtraSaque = limiteExtraSaque;
     }
 
-    
+    @Override
+    public double valorTotalPermitidoSaque(){
+        double valorTotalPermitidoSaque = getLimiteExtraSaque() + getSaldoEmConta();
+        return valorTotalPermitidoSaque;
+    }
+
+
+    public void calculoLimiteSaque(){
+        setLimiteExtraSaque(getSaldoEmConta()/2);
+        //double valorTotalPermitidoSaque = getLimiteExtraSaque() + getSaldoEmConta();
+
+        System.out.println("Seu limite especial é de R$ " +getLimiteExtraSaque());
+        System.out.println("Você pode sacar no máximo R$ " +valorTotalPermitidoSaque());
+        //ex.: ana tem 300, seu limite extra é de 150, seu saque máximo permitido é de 450
+    }
+
+
+
 }
