7615acbef123da23449a5f231a812299de85e3c4
Chrystina <144075668+chryssstina@users.noreply.github.com>
2025-02-10 10:16:51 -0300

    Update and rename Main (1).java to App.java

--- sistema-de-logistica-de-transporte-terrestre - Copia/App.java
+++ sistema-de-logistica-de-transporte-terrestre - Copia/App.java
@@ -1,7 +1,7 @@
 
 //package LogisticaTransporte;
 
-public class Main {
+public class App {
     public static void main(String[] args){
         Rota rota1 = new Rota(500, "ruim", "3");
         Manutencao manu = new Manutencao("05/10/2000", 200.00, "Troca do oleo");
