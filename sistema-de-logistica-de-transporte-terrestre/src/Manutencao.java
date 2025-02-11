b2d8e2f4d6e40b1f9de24c64c1c3f4865ff06723
viniciusbeta555 <96143281+viniciusbeta555@users.noreply.github.com>
2025-02-11 08:47:54 -0300

    Update Manutencao.java

--- sistema-de-logistica-de-transporte-terrestre/src/Manutencao.java
+++ sistema-de-logistica-de-transporte-terrestre/src/Manutencao.java
@@ -1,17 +1,22 @@
+//package LogisticaTransporte;
 
-public class Manutencao {
+public class Manutencao implements Notificacao{
     private String data;
     private double custo;
     private String tipo;
+    private Veiculo veiculo;
 
-    public Manutencao(String data, double custo, String tipo) {
+    public Manutencao(String data, double custo, String tipo, Veiculo veiculo) {
         this.data = data;
         this.custo = custo;
         this.tipo = tipo;
+        this.veiculo = veiculo;
     }
     
     //Talvez adicionar uma ligaçao com o veiculo que foi feita a manutençao
     public void detalhesManutencao(){
+        System.out.println("------------------------------");
+        System.out.println("O veiculo de placa "+veiculo.getPlaca()+" se encontgra em manutenção.");
         System.out.println("Data da manutenção: "+data);
         System.out.println("O custo foi de: "+custo+" reais");
         System.out.println("O tipo de manutenção feita foi "+tipo);
@@ -40,6 +45,20 @@ public class Manutencao {
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-    
-    
+
+    public void notifica(String remetenteEmail, String titulo){
+        System.out.println("------------------------------");
+        System.out.println("De: "+remetenteEmail);
+        System.out.println("Titulo: "+titulo);
+        System.out.println("Veiculo se encontra na manutenção por causa de: "+tipo);
+        //System.out.println("------------------------------");
+    }
+
+    public void notifica(int numeroRemetente){
+        System.out.println("------------------------------");
+        System.out.println("Mensagem de: "+numeroRemetente);
+        System.out.println("Veiculo na manutenção por causa de: "+tipo);
+        //System.out.println("------------------------------");
+    }
+
 }
