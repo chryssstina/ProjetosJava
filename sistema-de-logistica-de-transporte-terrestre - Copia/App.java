5ce66b5a325a26d06bd2b4e4526ac9d67c435ba7
Chrystina <144075668+chryssstina@users.noreply.github.com>
2025-02-11 07:20:55 -0300

    Delete sistema-de-logistica-de-transporte-terrestre - Copia directory

--- sistema-de-logistica-de-transporte-terrestre - Copia/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        CaminhaoLeve caminhao1 = new CaminhaoLeve("ABC123", "2 toneladas", "Diesel", 5.0, 6.0, "Frigo");
-        caminhao1.exibirInformacoes();
-        caminhao1.custoManutencao();
-        
-        
-    }
-}
