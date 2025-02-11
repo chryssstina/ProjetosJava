f59e1f531dd8afeae5120b8c04bb9b67c6345b57
Chrystina <144075668+chryssstina@users.noreply.github.com>
2025-02-11 08:11:51 -0300

    Update App.java
    Criando os objetos caminhao1 e funcionario1 para testes

--- sistema-de-logistica-de-transporte-terrestre/src/App.java
+++ sistema-de-logistica-de-transporte-terrestre/src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        CaminhaoLeve caminhao1 = new CaminhaoLeve("ABC123", "2 toneladas", "Diesel", 5.0, 6.0, "Frigo");
+        caminhao1.exibirInformacoes();
+        caminhao1.custoManutencao();
+        
+        System.out.println();
+        Funcionario funcionario1 = new Funcionario("João", "123.456.789.10", "Operador Logístico");
+        funcionario1.exibirInformacoes();
     }
 }
