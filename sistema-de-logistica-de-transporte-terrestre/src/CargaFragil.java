d98789304abc020e74cc7204a402320a4edad038
viniciusbeta555 <96143281+viniciusbeta555@users.noreply.github.com>
2025-02-17 10:27:35 -0300

    Update CargaFragil.java
    herda metodo inforCarga

--- sistema-de-logistica-de-transporte-terrestre/src/CargaFragil.java
+++ sistema-de-logistica-de-transporte-terrestre/src/CargaFragil.java
@@ -11,9 +11,19 @@ public class CargaFragil extends Carga{
     
     @Override
     public void valorSeguro() {
-        
+        double seguro = this.valor*0.25;
+        System.out.println("O valor do seguro da carga "+item+" é de "+seguro+" reais.");
     }
 
+    @Override
+    public void inforCarga() {
+       System.out.println("Carga Frágil:");
+       System.out.println("Tipo da Carga: "+item);
+       System.out.println("Quantidade Transportada: "+quantidade);
+       System.out.println("Peso Transportado: "+peso);
+       System.out.println("Valor da Carga Acrescentado do Seguro: "+((this.valor*0.25)+valor));
+       System.out.println("--------------------------");
+    }
     
 
     public String getItem() {
@@ -31,6 +41,7 @@ public class CargaFragil extends Carga{
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
-    
+
+   
     
 }
