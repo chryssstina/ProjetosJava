ee818f63e65b2456d8de5fc9a37afd28a5817473
viniciusbeta555 <96143281+viniciusbeta555@users.noreply.github.com>
2025-02-17 10:28:02 -0300

    Update CargaPerigosa.java
    herda metodo inforCarga

--- sistema-de-logistica-de-transporte-terrestre/src/CargaPerigosa.java
+++ sistema-de-logistica-de-transporte-terrestre/src/CargaPerigosa.java
@@ -1,12 +1,44 @@
 public class CargaPerigosa extends Carga{
 
-    public CargaPerigosa(double peso, String tipoCarga, double valor) {
+    private String item;
+    private int quantidade;
+
+    public CargaPerigosa(double peso, String tipoCarga, double valor, String item, int quantidade) {
         super(peso, tipoCarga, valor);
+        this.item = item;
+        this.quantidade = quantidade;
     }
     
     @Override
     public void valorSeguro() {
-        
+        double seguro = this.valor*0.75;
+        System.out.println("O valor do seguro da carga "+item+" é de "+seguro+" reais.");
+    }
+
+    @Override
+    public void inforCarga() {
+        System.out.println("Carga Perigosa:");
+        System.out.println("Tipo da Carga: "+item);
+        System.out.println("Quantidade Transportada: "+quantidade);
+        System.out.println("Peso Transportado: "+peso);
+        System.out.println("Valor da Carga Acrescentado do Seguro: "+((this.valor*0.75)+valor));
+        System.out.println("--------------------------");
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
     
 }
